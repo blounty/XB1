2897abda4c395c8094a5a7073418c9819e8d608b
Alex Blount <alex.blount@re-systems.co.uk>
2013-05-22 09:32:12 +0100

    initial commit

---
+++ XboxOne.WP8/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace XboxOne.WP8.Views
+{
+    public partial class HomeView : MvxPhonePage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
